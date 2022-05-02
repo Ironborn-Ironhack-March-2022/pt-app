65bff39d8c936db4197f0cbe1a874f4e1f23bd7e
Louise <louisegabriellecc@gmail.com>
2022-05-02 16:12:14 +0100

    Client routes added

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -1,4 +1,31 @@
 const router = require("express").Router();
+
 const Client = require("../models/Client.model");
+const User = require("../models/User.model");
+
+const isLoggedIn = require("../middleware/isLoggedIn");
+const isClient = require("../middleware/isClient");
 
 module.exports = router;
+
+//Homepage
+router.get("/homepage", isLoggedIn, isClient,(req, res, next) => {
+    res.render("clients/client-homepage");
+});
+
+//Profile
+router.get("/profile/", isLoggedIn, isClient, (req, res, next) => {
+    console.log(req.session.user.id)
+    res.render("clients/client-profile");
+});
+
+//Tasks-list
+router.get("/tasks", isLoggedIn, isClient, (req, res, next) => {
+    res.render("clients/client-tasks");
+});
+
+//Exercise-list
+router.get("/exercises", isLoggedIn, isClient, (req, res, next) => {
+    res.render("clients/exercises-list");
+});
+
