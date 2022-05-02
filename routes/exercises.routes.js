6b0eea6412b16892c232266d80942a3991192021
Ruben Poelen <ruben.poelen@hotmail.com>
2022-05-02 17:52:00 +0200

    Added an upload to the exercises-create view. Its working. Added code in exercises.routes.js to export said image. get list of exercises and create exercises.

--- routes/exercises.routes.js
+++ routes/exercises.routes.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
 const isLoggedIn = require("../middleware/isLoggedIn");
+const path = require("path");
+const cloudinary = require("../config/cloudinary.config");
+
+// const storage = cloudinary.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/images/exercises");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = cloudinary({ storage: storage });
 const Exercise = require("../models/Exercise.model");
 
 //Exercises list
@@ -19,15 +32,22 @@ router.get("/create", (req, res, next) => {
   res.render("exercises/create-exercises");
 });
 
-router.post("/exercises/create", (req, res, next) => {
+router.post("/create", cloudinary.single("file"), (req, res, next) => {
+  console.log(req.file);
+  if (!req.file) {
+    const error = new Error("Please upload a file");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
   const newInfo = {
     name: req.body.name,
     category: req.body.category,
-    image: req.body.image,
     description: req.body.description,
+    image: req.file.path,
     reps: req.body.reps,
     sets: req.body.sets,
   };
+
   Exercise.create(newInfo)
     .then(() => {
       res.redirect("/exercises");
