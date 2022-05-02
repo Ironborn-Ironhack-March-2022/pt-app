492fe810ed0d71c1fded8268db6af72eb0788ab9
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-05-02 17:00:49 +0200

    added instructor routes

--- routes/instructor.routes.js
+++ routes/instructor.routes.js
@@ -1,4 +1,31 @@
 const router = require("express").Router();
-const Instructor = require("../models/Instructor.model");
+const User = require("../models/User.model");
 
+
+
+
+
+
+router.get("/profile", (req, res, next) => {
+    User.findById(req.session.user._id)
+        .then((instructorDetails) => {
+            res.render("instructors/instructor-profile.hbs", {instructor: instructorDetails})
+        })
+        .catch(err => {
+            next(err)
+        })
+  });
+
+router.get("/:instructorId/client-list", (req, res, next) => {
+    console.log(req.params)
+    User.findById(req.params.instructorId)
+        .then( (instructorDetails) => {
+            console.log(instructorDetails)
+            res.render("instructors/clients-list.hbs")
+        })
+})
+
+router.get("/:instructorId/workouts", (req, res, next) => {
+    res.render("instructors/instructor-workout.hbs")
+})
 module.exports = router;
