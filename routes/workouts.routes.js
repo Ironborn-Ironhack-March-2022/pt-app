308265a7cf1548bc04368cb9ddaad5a28ff7f2e4
Louise <louisegabriellecc@gmail.com>
2022-05-03 18:50:29 +0100

    Restarting on workouts

--- routes/workouts.routes.js
+++ routes/workouts.routes.js
@@ -2,40 +2,53 @@ const router = require("express").Router();
 const Workout = require("../models/Workout.model");
 const Exercise = require("../models/Exercise.model");
 
-// Create new workout - render form
-router.get("/:userId/create-new-workout", (req, res, next) => {
+// Create new workout - render form - COMMENTED OUT AS TRYING SOMETHING ELSE
+// router.get("/:userId/create-new-workout", (req, res, next) => {
+//   console.log(
+//     "create new workout was clicked >>>>>>>>>>>>>>>>",
+//     req.params.userId
+//   );
+//   Exercise.find()
+//     .then((exerciseDetails) => {
+//       res.render("workouts/create-new-workout", { exercise: exerciseDetails });
+//     })
+//     .catch((err) => {
+//       console.log("Error finding exercises on the DB", err);
+//     });
+// });
+
+// Create new exercise - process form - COMMENTED OUT AS TRYING SOMETHING ELSE
+// router.post("/:userId/create-new-workout", (req, res, next) => {
+//   const newInfo = {
+//     exerciseObject: {
+//       properties: [
+//         {
+//           exercises: {
+//             _id: req.body.exercises,
+//           },
+//         },
+//       ],
+//     },
+//     description: req.body.description,
+//   };
+//   Workout.create(newInfo)
+//     .populate("exerciseObject")
+//     .then((newWorkout) => {
+//       console.log(newWorkout);
+//       res.redirect("/homepage");
+//     });
+// });
+
+//Create new workout - render form 
+router.get("/create-new-workout", (req, res, next) => {
   console.log(
-    "create new workout was clicked >>>>>>>>>>>>>>>>",
-    req.params.userId
-  );
+    "create new workout was clicked >>>>>>>>>>>>>>>>"  );
   Exercise.find()
     .then((exerciseDetails) => {
-      res.render("workouts/create-new-workout", { exercise: exerciseDetails });
+      res.render("workouts/create-new-workout", {exercise: exerciseDetails });
     })
     .catch((err) => {
       console.log("Error finding exercises on the DB", err);
-    });
-});
-
-// Create new exercise - process form
-router.post("/:userId/create-new-workout", (req, res, next) => {
-  const newInfo = {
-    exerciseObject: {
-      properties: [
-        {
-          exercises: {
-            _id: req.body.exercises,
-          },
-        },
-      ],
-    },
-    description: req.body.description,
-  };
-  Workout.create(newInfo)
-    .populate("exerciseObject")
-    .then((newWorkout) => {
-      console.log(newWorkout);
-      res.redirect("/homepage");
     });
 });
 
