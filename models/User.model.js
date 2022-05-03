b332c5e6cf98a1d9ba248e0c1afe99fe9f83e468
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-05-03 12:13:12 +0200

    added add clients to instructor routes

--- models/User.model.js
+++ models/User.model.js
@@ -29,7 +29,7 @@ const userSchema = new Schema(
     },
     instructor: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "Instructor",
     },
     clients: [{
       type: mongoose.Schema.Types.ObjectId,
