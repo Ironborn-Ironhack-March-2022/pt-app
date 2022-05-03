eef5dd43f821eab1bab45b7fd891f29091e5fb0e
Ruben Poelen <ruben.poelen@hotmail.com>
2022-05-03 14:08:35 +0200

    Please enter the commit message for your changes. Lines starting

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
+const isLoggedIn = require("../middleware/isLoggedIn");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/homepage", (req, res, next) => {
-  if (req.session.user.role === "Client") {
+router.get("/homepage", isLoggedIn, (req, res, next) => {
+  if (!req.session.user.role === "Client") {
     return res.redirect("/client/homepage");
   } else if (req.session.user.role === "Instructor") {
     return res.redirect("/instructor/homepage");
