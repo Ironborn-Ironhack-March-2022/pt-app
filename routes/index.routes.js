3ff143dd3dfa8d831d0eb20ec96409e3fc2ff5b5
Ruben Poelen <ruben.poelen@hotmail.com>
2022-05-03 12:48:24 +0200

    added correct functionality to navbar-homepage. the other still need designated pages

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,4 +5,12 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get("/homepage", (req, res, next) => {
+  if (req.session.user.role === "Client") {
+    return res.redirect("/client/homepage");
+  } else if (req.session.user.role === "Instructor") {
+    return res.redirect("/instructor/homepage");
+  }
+});
+
 module.exports = router;
