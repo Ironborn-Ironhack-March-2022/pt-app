07229b2f2b213400c2fc665adecf99fbebce2c6e
Louise <louisegabriellecc@gmail.com>
2022-05-03 11:26:45 +0100

    Workout model updated

--- models/Workout.model.js
+++ models/Workout.model.js
@@ -1,30 +1,21 @@
 const { default: mongoose } = require("mongoose");
 const { Schema, model } = require("mongoose");
 
-const workoutSchema = new Schema({
-  catagory: {
-    type: [String],
-    enum: [
-      "Olymic Lifting",
-      "Yoga",
-      "Cardio",
-      "Interval",
-      "Weight Lifting",
-      "CrossFit",
-      "Mix",
-    ],
-  },
-  description: String,
-  sets: {
-    type: Number,
-    enum: [1, 2, 3, 4, 5],
-  },
+const workoutSchema = new Schema(
+  {
   exercises: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Exercise",
     },
   ],
+  description: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  feedback: String,
+  completed: Boolean,
 });
 
 const Workout = model("Workout", workoutSchema);
